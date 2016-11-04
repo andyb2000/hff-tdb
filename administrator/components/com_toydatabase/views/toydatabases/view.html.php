103da8c7a42e5a6116b54e0aa9db1ed23b24c786
Andy Brown <andy@onyx.net>
2016-11-04 10:06:25 +0000

    fix

--- administrator/components/com_toydatabase/views/toydatabases/view.html.php
+++ administrator/components/com_toydatabase/views/toydatabases/view.html.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class ToyDatabaseViewToyDatabase extends JViewLegacy
+class ToyDatabaseViewToyDatabases extends JViewLegacy
 {
 	/**
 	 * View form
