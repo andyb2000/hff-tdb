802b214086dfa501e1e38ac75513d466af1cf521
Andy Brown <andy@broadcast-tech.co.uk>
2016-11-06 14:04:24 +0000

    fix backslashes for folders

--- components/com_toydatabase/toydatabase_livesearch.php
+++ components/com_toydatabase/toydatabase_livesearch.php
@@ -1,6 +1,6 @@
 <?php
 define( '_JEXEC', 1 );
-define('JPATH_BASE', dirname(__FILE__)."../../" );//this is when we are in the root,means path to Joomla installation
+define('JPATH_BASE', dirname(__FILE__)."/../.." );//this is when we are in the root,means path to Joomla installation
 define( 'DS', DIRECTORY_SEPARATOR );
 
 require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
