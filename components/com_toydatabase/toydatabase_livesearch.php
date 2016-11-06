16127e846bb4b28ef073e42a1b05447c352a212e
Andy Brown <andy@broadcast-tech.co.uk>
2016-11-06 14:01:12 +0000

    toydb folder path

--- components/com_toydatabase/toydatabase_livesearch.php
+++ components/com_toydatabase/toydatabase_livesearch.php
@@ -3,8 +3,8 @@ define( '_JEXEC', 1 );
 define('JPATH_BASE', dirname(__FILE__) );//this is when we are in the root,means path to Joomla installation
 define( 'DS', DIRECTORY_SEPARATOR );
 
-require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
-require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+require_once ( JPATH_BASE .DS.'../includes'.DS.'defines.php' );
+require_once ( JPATH_BASE .DS.'../includes'.DS.'framework.php' );
 
 $app = JFactory::getApplication('site');
 $app->initialise();
