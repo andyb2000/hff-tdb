e7c9a831c73f56d386696c7fea92c5beb6b9f6db
Andy Brown <andy@onyx.net>
2016-11-29 11:48:56 +0000

    debug array

--- administrator/components/com_toydatabase/toydatabase.php
+++ administrator/components/com_toydatabase/toydatabase.php
@@ -433,6 +433,7 @@ switch($act) {
 						// set category query too
 						if (is_array($frm_in_toycat_arr)) {
 							echo "DEBUG array found<BR>\n";
+							print_r($frm_in_toycat_arr);
 							foreach ($frm_in_toycat_arr as $toycat_human_val) {
 								echo "DEBUG loop: $toycat_human_val<BR>\n";
 								$ins_cat_request = $db->getQuery(true);
