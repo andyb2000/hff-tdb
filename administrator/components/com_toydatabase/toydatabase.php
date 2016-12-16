7c7135cf0a02643712f6c6f595d928cb48153fe1
Andy Brown <andy@onyx.net>
2016-12-16 15:22:29 +0000

    membership category lookup

--- administrator/components/com_toydatabase/toydatabase.php
+++ administrator/components/com_toydatabase/toydatabase.php
@@ -1198,7 +1198,17 @@ foreach ($membershiptypes_rows as $membershiptypes_output) {
 					</tr>
 					<tr>
 					<td valign=top><B>Membership Category :</B></td>
-					<td><input type=text size=10 name='in_memb_category' value='<?=$row["memb_category"]?>'></td>
+					<td><input type=text size=10 name='in_memb_category' value='<?=$row["memb_category"]?>'>
+					<select name='in_memb_category'/>
+<option value=''></option>
+<option value='1' <?php if ($row["memb_category"] == "1") {echo "checked";}; ?>>Individual / Family</option>
+<option value='2' <?php if ($row["memb_category"] == "2") {echo "checked";}; ?>>Voluntary Organisation</option>
+<option value='3' <?php if ($row["memb_category"] == "3") {echo "checked";}; ?>>Schools</option>
+<option value='4' <?php if ($row["memb_category"] == "4") {echo "checked";}; ?>>Childcare settings</option>
+<option value='5' <?php if ($row["memb_category"] == "5") {echo "checked";}; ?>>Childminders</option>
+<option value='6' <?php if ($row["memb_category"] == "6") {echo "checked";}; ?>>Organisations outside of Hartlepool</option>
+</select>
+					</td>
 					</tr>
 					<tr>
 					<td valign=top><B>Renewal Date :</B></td>
