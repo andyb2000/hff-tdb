abb6e8f5a58f8c55c41bb0f1a2aa700780fc4ccd
Andy Brown <andy@onyx.net>
2017-01-26 12:15:23 +0000

    new display style

--- administrator/components/com_toydatabase/pdf_output.php
+++ administrator/components/com_toydatabase/pdf_output.php
@@ -533,76 +533,34 @@ switch($disp) {
 		
 		$db->setQuery($query);
 		$row = $db->loadAssocList('a.id');
+		if (!empty($row)) {
+			// print_r($row);
+			foreach ($row as $row_key=>$row_value) {
 		?>
 		<table width=96% border=1 cellpadding=0 cellspacing=0>
-		<tr valign=top aligh=left><td valign=top>photo</td><td valign=top>
+		<tr valign=top aligh=left><td valign=top><?php
+		// check the file exists to display the image
+		if (is_file(JPATH_BASE."/".$row_value["picture"])) {
+			// dynamically resize image using php
+			echo "<img src='".JURI::root()."/../../../../components/com_toydatabase/toydatabase_thumbnailer.php?img=".$row_value["picture"]."' alt='".$row_value["picture"]."'>";
+		} else {
+			echo "Sorry no image exists";
+		};
+		?></td><td valign=top>
 			<table width=100% border=0 cellpadding=0 cellspacing=0>
-				<tr valign=top><td width=100>Title</td></tr>
-				<tr valign=top><td width=100>URN</td></tr>
-				<tr valign=top><td width=100%>Description<BR>goes<br>here<br></td></tr>
+				<tr valign=top><td width=100><?=$row_value["a.name"]?></td></tr>
+				<tr valign=top><td width=100><?=$row_value["a.urn"]?></td></tr>
+				<tr valign=top><td width=100%><?=$row_value["a.description"]?></td></tr>
 			</table>
 		</td></tr>
 		</table>
-				<table width=85% border=1 cellpadding=0 cellspacing=0>
-				<tr><td width=30%><B>Toy name</B></td>
-				<td width=30%><B>Toy category</B></td>
-				<td width=30%><B>Toy Photo (small)</B></td>
-				<td width=10%><B>Status</B></td></tr>
-				<?php 
-				if (!empty($row)) {
-					// print_r($row);
-					foreach ($row as $row_key=>$row_value) {
-						echo "<tr>";
-						echo "<td>".$row_value["a.name"]."</td>\n";
-						echo "<td>";
-						// Now retrieve the category (ies)
-//						$query_category = $db->getQuery(true);
-//						$query_category
-//						->select(array('a.*','b.category'))
-//						->from($db->quoteName('#__toydatabase_categorylink','a'))
-//						->join('INNER', $db->quoteName('#__toydatabase_equipment_category', 'b') . ' ON (' . $db->quoteName('a.categoryid') . ' = ' . $db->quoteName('b.id') . ')')
-//						->where($db->quoteName('a.equipmentid') . ' = '. $row_key);
-//						$db->setQuery((string) $query_category);
-//						$db->execute();
-//						$category_rows = $db->loadAssocList();
-//						foreach ($category_rows as $cat_display) {
-//							echo $cat_display["category"]."<BR>\n";
-//						};
-						echo "</td>";
-						echo "<td>";
-						// check the file exists to display the image
-						if (is_file(JPATH_BASE."/".$row_value["picture"])) {
-							// dynamically resize image using php
-							echo "<img src='".JURI::root()."/../../../../components/com_toydatabase/toydatabase_thumbnailer.php?img=".$row_value["picture"]."' alt='".$row_value["picture"]."'>";
-						} else {
-							echo "Sorry no image exists";
-						};
-						echo "</td>\n";
-						echo "<td>";
-						switch($row_value["status"]) {
-							case "3":
-								echo "Damaged/No longer available";
-								break;
-							case "2":
-								echo "Awaiting cleaning/repair";
-								break;
-							case "1":
-								echo "On Loan";
-								break;
-							case "0":
-								echo "Available";
-								break;
-							default:
-								echo "Unknown";
-								break;
-						};
-						echo "</td>\n";
-						echo "</tr>";
+<?php 
 					};
 				} else {
 					// no rows or toys in database found
 					echo "<tr><td colspan=4 align=center><B>Sorry - No items found</B></td></tr>\n";
 				};
+				echo "<BR><hr width=100%><BR><BR>\n";
 			};
 		};
 				?>
